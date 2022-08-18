4cf775a8acc79401f8d24861fcdd9f5c4c7b5351
jenny <jenny.rigsjo@live.com>
2022-08-18 14:59:07 +0200

    Remove unneeded code to activate session

--- MVCBasics/Program.cs
+++ MVCBasics/Program.cs
@@ -5,11 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc();
 
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
-});
-
 var app = builder.Build();
 
 app.UseStaticFiles();
